df9ac869f2348802cccbfb539c179268e3b47ce2
wildan ismail <wildanwork86@gmail.com>
2022-07-21 14:56:22 +0700

    pagination done

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 const path = require('path')
 const bodyParser = require('body-parser')
-const fs = require('fs')
-const { json } = require('body-parser')
-const read = fs.readFileSync('siswa.json','utf-8')
-const data = JSON.parse(read)
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('bread.db', sqlite3.OPEN_READWRITE, err => {
+  if (err) {
+    console.log('gagal koneksi dengan database', err)
+  }
+})
 const moment = require('moment')
+const { Console } = require('console')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -19,32 +22,61 @@ app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
-  const read = fs.readFileSync('siswa.json','utf-8')
-const data = JSON.parse(read)
-  res.render('list', { rows: data,moment })
+  const page = req.query.page || 1
+  const LIMIT = 3;
+  const OFFSET = (page - 1) * LIMIT
+  db.all('Select count (*) as total from bread', (err, data) => {
+    const pages = Math.ceil(data[0].total /LIMIT)
+    db.all('SELECT * FROM bread LIMIT ? OFFSET ?', [LIMIT, OFFSET], (err, data) => {
+
+      if (err) {
+        console.log('gabisa ambil data euy', err)
+      }
+      res.render('list', { rows: data, moment,pages,page })
+    })
+
+  })
+
+
 })
 app.get('/add', (req, res) => {
   res.render('add')
 })
 app.post('/add', (req, res) => {
-  const {string,integer,float,date,boolean} = req.body
-  data.push({string, integer: parseInt(integer), float: parseFloat(float), date, boolean: JSON.parse(boolean)})
-  fs.writeFileSync('siswa.json',JSON.stringify(data,null,3),'utf-8')
-  res.redirect('/')
+  const { string, integer, float, date, boolean } = req.body
+  db.run('insert into bread (_string,_integer,_float,_date,_boolean) values (?, ?, ?, ?, ?)', [string, (integer), (float), date, boolean], (err) => {
+    if (err)
+      console.log('gabisa ambil data', err)
+    res.redirect('/')
+  })
+
 })
 
-app.get('/delete/:id',(req,res)=>{
-const index = req.params.id   
-data.splice(index,1)
-fs.writeFileSync('siswa.json',JSON.stringify(data,null,3),'utf-8')
-res.redirect('/')
+app.get('/delete/:id', (req, res) => {
+  const index = req.params.id
+  db.run('delete from bread where _id = ?', [index], (err) => {
+    if (err)
+      console.log('gabisa ambil data', err)
+    res.redirect('/')
+  })
+
 })
-app.get('/edit/:id',(req,res)=> {
-res.render('edit',{item: data[req.params.id]})
+app.get('/edit/:id', (req, res) => {
+  db.all('select * from bread where _id = ?', [req.params.id], (err, data) => {
+    if (err)
+      console.log('gabisa ambil data', err)
+    res.render('edit', { item: data[0] })
+  })
+
 })
-app.post('/edit/:id',(req,res)=>{
-  data[req.params.id]  = ({string,integer,float,date,boolean}= req.body)
-  fs.writeFileSync('siswa.json',JSON.stringify(data,null,3),'utf-8')
+app.post('/edit/:id', (req, res) => {
+  data[req.params.id] = ({ string, integer, float, date, boolean } = req.body)
+  db.all('SELECT * FROM bread', (err, data) => {
+    if (err) {
+      console.log('gabisa ambil data euy', err)
+    }
+    res.render('list', { rows: data, moment })
+  });
   res.redirect('/')
 })
 
